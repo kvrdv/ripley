da05fb951b721ecc714e200b3aab2b500e06a2f2
KVRDV <s.kovardaev@gmail.com>
2021-06-25 16:36:32 +0300

    webpacked

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import './styles/styles.css';
+
+// Работа углового меню:
+const openCornerMenu = document.querySelector('.corner-menu-button');
+const closeCornerMenu = document.querySelector('.corner-menu-button-close');
+const cornerMenu = document.querySelector('.corner-menu');
+
+openCornerMenu.addEventListener('click', (event) => {
+  cornerMenu.classList.remove('hidden');
+  document.body.style.overflow = 'hidden';
+});
+
+closeCornerMenu.addEventListener('click', (event) => {
+  cornerMenu.classList.add('hidden');
+  document.body.style.overflow = 'auto';
+});
+
+// Работа элементов меню:
+const mainItemButton = document.querySelectorAll('.main-item-button');
+const mainItemButtonUnpressed = document.querySelectorAll(
+  '.main-item-button-unpressed'
+);
